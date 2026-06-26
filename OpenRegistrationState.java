5cb90d7f57df1da17ba0226fe5423064b3e576b1
ckbrow16 <ckbrow16@asu.edu>
2026-06-25 23:34:49 -0500

    Commented out exception in OpenRegistrationState.java

--- OpenRegistrationState.java
+++ OpenRegistrationState.java
@@ -2,7 +2,7 @@ public class OpenRegistrationState implements RegistrationState{
 
     @Override
     public void openRegistration(Race race) {
-        throw new IllegalStateException("Registration is already open.");
+       // throw new IllegalStateException("Registration is already open."); // need to implement catch
     }
 
     @Override
